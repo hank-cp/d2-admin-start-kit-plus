0ccfc47723eec0546819c6b2678053e6871af30a
Hank CP <true.cp@gmail.com>
2020-05-06 19:11:14 +0800

    update mockAdapter config

--- src/d2admin/plugin/axios/mock-loader.js
+++ src/d2admin/plugin/axios/mock-loader.js
@@ -1,7 +1,9 @@
 import axios from './index'
 import MockAdapter from 'axios-mock-adapter'
 
-const mockAdapter = new MockAdapter(axios)
+const mockAdapter = new MockAdapter(axios, {
+  onNoMatch: 'passthrough'
+})
 
 const req = context => context.keys().map(context)
 const mockers = req(require.context('@/module', true, /mock\.(js|ts)$/))
