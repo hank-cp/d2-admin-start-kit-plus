7abc0b48e1b2d93354f405568daadde2bda587c5
Hank CP <true.cp@gmail.com>
2020-05-07 17:26:25 +0800

    global error catcher

--- src/d2admin/plugin/error/index.js
+++ src/d2admin/plugin/error/index.js
@@ -4,26 +4,26 @@ import util from '@/d2admin/libs/util'
 export default {
   install(Vue, options) {
     function writeLog(logType) {
-      return (error, instance, info) => {
+      return (error, vm, info = '') => {
         Vue.nextTick(() => {
           // store 追加 log
           store.dispatch('d2admin/log/push', {
-            message: `${info}: ${error.message}`,
+            message: `${info}: ${(typeof error === 'object') ? error.message : error}`,
             type: logType,
             meta: {
               error,
-              instance
+              vm
             }
           })
           // 只在开发模式下打印 log
           if (process.env.NODE_ENV === 'development') {
-            util.log.capsule('D2Admin', 'ErrorHandler', 'danger')
+            util.log.capsule('D2Admin', 'ErrorHandler', logType)
             util.log.danger('>>>>>> 错误信息 >>>>>>')
             console.log(info)
             util.log.danger('>>>>>> Vue 实例 >>>>>>')
-            console.log(instance)
+            console.log(vm)
             util.log.danger('>>>>>> Error >>>>>>')
-            console.log(error)
+            console.trace(error)
           }
         })
       }
@@ -32,5 +32,26 @@ export default {
       Vue.config.warnHandler = writeLog('warning')
     }
     Vue.config.errorHandler = writeLog('danger')
+
+    window.onunhandledrejection = error => {
+      store.dispatch('d2admin/log/push', {
+        message: (('message' in error.reason) && error.reason.message) || 'Unknown error',
+        type: 'danger',
+        meta: {
+          error: error.reason,
+          trace: 'stack' in error.reason && error.reason.stack
+        }
+      })
+    }
+    window.onerror = (event, source, lineno, colno, error) => {
+      store.dispatch('d2admin/log/push', {
+        message: (error && error.message) || 'Unknown error',
+        type: 'danger',
+        meta: {
+          error,
+          trace: error.stack
+        }
+      })
+    }
   }
 }
